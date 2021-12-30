c490c99dd95ed3077f2d8a2898689c3665429805
Hardik Naik <hardiknaik7444@gmail.com>
2021-12-30 23:45:23 +0530

    Updates

--- _next/static/1rtmRPfVv_N0oIQ9vdmiB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/794-04052a3e10559c2e.js","static/css/b6e87d2e24303ad3.css","static/chunks/pages/index-db873090b8e5c68b.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
